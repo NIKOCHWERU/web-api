efe54acabce4bf7ad0cf95f2fe5895b6dbb398ce
Niko Admin <niko.narasumberhukum@gmail.com>
2026-06-27 22:40:03 +0700

    Move all public API routes from web.php to api.php to avoid CSRF 419 error

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,22 @@ Route::get('/user', function (Request $request) {
 Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/articles/{slug}', [ArticleController::class, 'show']);
 Route::post('/contact', [ContactController::class, 'store']);
+
+// Get categories
+Route::get('/categories', function () {
+    return response()->json(\App\Models\Category::withCount('articles')->get());
+});
+
+// Track article view
+Route::post('/articles/{slug}/view', function (string $slug) {
+    $article = \App\Models\Article::where('slug', $slug)
+                    ->where('status', 'published')
+                    ->first();
+
+    if ($article) {
+        $article->incrementViews();
+        return response()->json(['views' => $article->views]);
+    }
+
+    return response()->json(['error' => 'Not found'], 404);
+});
